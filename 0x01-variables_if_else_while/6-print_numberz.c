7f578f11f1d635803a54075a471f674d75c7f384
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-11 12:43:29 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0 when success else 1
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		++i;
+	}
+	putchar('\n');
+	return (0);
+}
+
