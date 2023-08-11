41bbbb5db501436bb2f8bd42418dfad1e37a6704
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-11 11:41:42 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0 when success else 1
+ */
+int main(void)
+{
+	for (char i = 97; i < 123; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
