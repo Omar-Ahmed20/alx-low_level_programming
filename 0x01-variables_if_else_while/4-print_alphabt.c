38769fd8bca25f1fc96b09f05c731522c70a9464
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-11 12:22:36 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0 when success else 1
+ */
+int main(void)
+{
+	char i = 97;
+
+	while (i < 123)
+	{
+		if (i == 113 || i == 101)
+			continue;
+		else
+		{
+			putchar(i);
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
