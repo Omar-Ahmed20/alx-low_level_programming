b77acb4119fa21e43e7875075f2faff0db6b7686
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-11 12:32:40 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0 when success else 1
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 11)
+	{
+		printf(i);
+	}
+	putchar('\n');
+	return (0);
+}
