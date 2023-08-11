da11d9e108f9babd5b6a8c668172bfbfab842ba8
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-11 13:32:15 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *Return: 0 when success else 1
+ */
+int main(void)
+{
+	char i = 49;
+
+	while (i < 103)
+	{
+		putchar(i);
+		if (i == 57)
+			i += 40;
+		else
+			i++;
+	}
+	putchar('\n');
+	return (0);
+}
