ddbca0cc1aeb1f5cac852699da9530df5221326a
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-15 14:05:58 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - chech if its letter
+ * @c: user input
+ *Return: 1 if letter 0 if not
+ */
+int _isalpha(int c);
+{
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+		return (1);
+	else
+		return (0);
+}
