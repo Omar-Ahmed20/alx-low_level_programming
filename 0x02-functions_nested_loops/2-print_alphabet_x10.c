abf5a161d71f20e2489416e0d4139798019ddf4b
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-15 13:25:27 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints alphabets 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char i = 10;
+
+	while (i < 10)
+	{
+		char j = 97;
+
+		while (j < 123)
+		{
+			_putchar(j);
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
