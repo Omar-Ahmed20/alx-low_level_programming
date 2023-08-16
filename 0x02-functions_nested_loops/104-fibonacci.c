838ce8c1f69b8e10d5947f9c7149fd44d2d71ee3
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-15 18:43:24 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main- main function fabnoci numbers
+ * Return: 0 when success
+ */
+int main(void)
+{
+	unsigned long a = 1;
+	unsigned long b = 2;
+	unsigned long c;
+	int count = 2;
+
+	printf("%ld, %ld, ", a, b);
+	while (count < 98)
+	{
+		c = a + b;
+		printf("%ld", c);
+		if (count != 49)
+			printf(", ");
+		a = b;
+		b = c;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
+
