f1665387bf07c9744422528d81838d1da1c95399
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-15 17:19:18 -0700

    task 11.12

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * print_to_98 - prints from n to 98
+ * @n: user input
  */
 void print_to_98(int n)
 {
@@ -11,20 +11,20 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			printf("%d, ",n);
+			printf("%d, ", n);
 			n++;
 		}
-		printf("%d\n",98);
+		printf("%d\n", 98);
 	}
 	else if (n > 98)
 	{
 		while (n > 98)
 		{
-			printf("%d, ",n);
+			printf("%d, ", n);
 			n--;
 		}
-		printf("%d\n",98);
+		printf("%d\n", 98);
 	}
 	else
-		printf("%d\n",98);
+		printf("%d\n", 98);
 }
