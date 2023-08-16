6eb06933398f29514e7fee98444300ab17bf4911
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-15 17:05:51 -0700

    task 11.7

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,9 +7,24 @@
  */
 void print_to_98(int n)
 {
-	while (n < 99)
+	if (n < 98)
 	{
-		printf("%d, ",n);
-		n++;
+		while (n < 98)
+		{
+			printf("%d, ",n);
+			n++;
+		}
+		printf("%d",98);
 	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ",n");
+			n--;
+		}
+		printf("%d",98);
+	}
+	else
+		printf("%d",98);
 }
