40aca189439e001f930f11f48ca7bd5fe484ac80
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-15 17:33:12 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *
+ *
+ */
+int main(void)
+{
+	int i = 1024;
+	int sum = 0;
+	while (i > 0)
+	{
+		if ((i % 3  == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+		i--;
+	}
+	printf("%d",sum);
+}
