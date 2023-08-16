15e83060f73adf1fa101749bcd542b687247ff48
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-15 18:05:32 -0700

    task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - times table all
+ *
+ */
+void print_times_table(int n)
+{
+	int i = 0;
+	int j = 1;
+	int re;
+	
+	if (n <= 15 && n >= 0)
+	{
+		while (i < n+1)
+		{
+			_putchar(48);
+			while (j < n+1)
+			{
+				_putchar(',');
+				_putchar(' ');
+				re = i * j;
+				if (re <= 9)
+					_putchar(' ');
+				if (re <= 99)
+					_putchar(' ');
+				if (re >= 100)
+				{
+					_putchar((re / 100) + 48);
+					_putchar((re / 10) + 48);
+				}
+				else if (re <= 99 && re >= 10)
+					_putchar((re / 10) + 48);
+				_putchar((re % 10) + 48);
+				j++;
+			}
+			i++;
+			j = 1;
+			_putchar('\n');
+		}
+	}
+}
+
