a8ce74963ec5c52f7d2777aa96ff2b1aa5204579
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-15 17:37:54 -0700

    task 13.3

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - sum of factorials
+ * Return: 0 when success
  */
 int main(void)
 {
 	int i = 1024;
 	int sum = 0;
+
 	while (i > 0)
 	{
 		if ((i % 3  == 0) || (i % 5 == 0))
@@ -15,6 +16,6 @@ int main(void)
 		}
 		i--;
 	}
-	printf("%d\n",sum);
+	printf("%d\n", sum);
 	return (0);
 }
