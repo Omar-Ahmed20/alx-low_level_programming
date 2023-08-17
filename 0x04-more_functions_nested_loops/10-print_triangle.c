2e590de81cbc2ddc26c8b1f7c1142c9153f21d1c
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-17 05:29:35 -0700

    task10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - prints inverted triangle
+ * @size: user input to determine the size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i = 0;
+
+	while (i < size)
+	{
+		int j = 0;
+
+		while (j < size)
+		{
+			if (j < size - i - 1)
+				printf(" ");
+			else
+				printf("#");
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (size <= 0)
+		printf("\n");
+}
