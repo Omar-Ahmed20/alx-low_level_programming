82ad56258ea91b5e41262f61a70a73873412ae52
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-21 14:17:34 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: input string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+		i++;
+	i--;
+	while (j < (i / 2))
+	{
+		char temp = s[j];
+
+		s[j] = s[i - j];
+		s[i - j] = temp;
+		j++;
+	}
+}
