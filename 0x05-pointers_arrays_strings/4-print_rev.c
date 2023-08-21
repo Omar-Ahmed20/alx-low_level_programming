b6039a98e0b0c3502ad066908a27bdb490bb2972
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-21 13:06:47 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse order
+ * @s: string input
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
