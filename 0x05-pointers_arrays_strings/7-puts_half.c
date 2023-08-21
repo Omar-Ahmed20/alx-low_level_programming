3586761c6cd06d0683d991e895566b2abe7de11f
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-21 15:12:39 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - print second half
+ * @str: string inpute
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	i++;
+	while (str[i / 2] != '\0')
+	{
+		_putchar(str[i / 2]);
+		i += 2;
+	}
+	_putchar('\n');
+}
