c20c94567bc883c11772c6f66ad93c853fe48ed2
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-24 07:34:16 -0700

    task 101.4

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,13 +7,17 @@
 
 void print_number(int n)
 {
+	unsigned int num;
+
+	num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + 48);
+	if (num / 10)
+		print_number(num / 10);
+	_putchar((num % 10) + 48);
 }
