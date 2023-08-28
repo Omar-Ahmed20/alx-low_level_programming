598c13cd04e0d145c18f80555b2f8053e9539b87
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-28 14:15:21 -0700

    task 5.1

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,38 +9,19 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
-	int k = 0;
-	int c = 0;
-	int j = 0;
-	int flag = 0;
-	int start = 0;
-
-	while (needle[k] != '\0')
-		k++;
-	while (haystack[i] != '\0')
+	while (*haystack != '\0')
 	{
-		if (haystack[i] == needle[0])
-		{
-			start = i;
-			c = i;
-			j = 0;
-			while (needle[j] != '\0')
-			{
-				if (needle[j] != haystack[c])
-				{
-					flag = 1;
-					break;
-				}
-				j++;
-				c++;
-			}
-		}
-		i++;
-	}
-	if (flag == 0 || (c - start) == k)
-		return (&haystack[start]);
-	else
-		return ('\0');
+		char *str1 = haystack;
+		char *str2 = needle;
 
+		while (*str1 == *str2 && *str2 != '\0')
+		{
+			str1++;
+			str2++;
+		}
+		if (*str2 == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return ('\0');
 }
