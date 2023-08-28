a24846e6a11c3357f075d1d3115fb7d17d414b06
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-28 10:36:29 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates charachter in a string
+ * @s: string input
+ * @c: the character
+ * Return: pointer to the c
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *p = 0;
+
+	while (s)
+	{
+		if (*s == c)
+		{
+			p = s;
+			break;
+		}
+		s++;
+	}
+	return (p);
+}
