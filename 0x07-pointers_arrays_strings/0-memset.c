ed37271c55c69bf68bcf3f9a5ea3929e293556c9
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-28 08:59:29 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fill n bytes of memory with b
+ * @s: string input
+ * @b: value to be filled with
+ * @n: number of bytes
+ * Return: pointer of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*s = b;
+		i++;
+		s++;
+	}
+	return (s);
+}
