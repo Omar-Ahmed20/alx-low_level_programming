fef05e2d5eb6165289d58c3f639e88e2d166e946
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-28 11:28:48 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,26 @@
 #include "main.c"
 
 /**
- *
- *
- *
+ * _strspn - gets the length of a prefix
+ * @s: string originl
+ * @accept: prefix
+ * Return: length of prefix
  */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
 
+	while (s[i] != '\0')
+	{
+		int j = 0;
+
+		while (s[i] != accept[j])
+		{
+			if (accept[j] == '\0')
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
