6a113e48ec52d97f4044aa3e020ebff11e63ac02
Omar Ahmed <omarahmedhu8@gmail.com>
2023-08-31 09:52:48 -0700

    task 101.1

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,15 +1,22 @@
 #include "main.h"
+
+/**
+ * wildcmp - match wild card string
+ * @s1: first string
+ * @s2: pattern to match with
+ * Return: 1 if match 0 if not
+ */
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
-		if(*s2 != '\0' && *s2 == '*')
-			return(wildcmp(s1, s2 + 1));
-		return(*s2 == '\0');
+		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == '\0');
 	}
 	if (*s2 == '*')
-		return(wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	else if (*s1 == *s2)
-		return (wildcmp(s1 + 1 , s2 + 1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	return (0);
 }
