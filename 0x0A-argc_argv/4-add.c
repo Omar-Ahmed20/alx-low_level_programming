b42d08c7b3e14204dc65b9c072744c151653c40a
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-01 14:11:48 -0700

    task 4 argv.1

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,13 +15,12 @@ int main(int argc, char *argv[])
 
 	while (i < argc)
 	{
-		if (atoi(argv[i]) > 0)
-			sum += atoi(argv[i]);
-		else
+		if (argv[i] < '0' || argv[i] > '9')
 		{
 			printf("Error\n");
 			return (1);
 		}
+		sum += atoi(argv[i]);
 		i++;
 	}
 	printf("%d\n", sum);
