ceff9de07eff9444fc8dddc50956e1d4247277a8
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-01 15:06:31 -0700

    task 100 argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 
 /**
- *
- *
+ * main - calculate the chenge of money
+ * @argc: number of arguments to main
+ * @argv: array of pointers to all arguments
+ * Return: 1 if error 0 if success
  */
 
 int main(int argc, char *argv[])
@@ -13,5 +15,24 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	if (
+	else
+	{
+		int i = 0, min = 0;
+		int money = atoi(argv[1]);
+		int cents[] = {25, 10, 5, 2, 1};
+
+		while (i < 5)
+		{
+			if (money >= cents[i])
+			{
+				min += money / cents[i];
+				if (money % cents[i] == 0)
+					break;
+				money = money % cents[i];
+			}
+			i++;
+		}
+		printf("%d\n", min);
+		return (0);
+	}
 }
