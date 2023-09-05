c6e01d493ef6d4cbc3a31c437efd14f02e0655f4
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-05 04:04:10 -0700

    task 0.1 0x0B

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,11 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *p;
+	char *p = malloc(sizeof(char) * size);
 	unsigned int i = 0;
 
 	if (size <= 0)
 		return (NULL);
-	p = malloc(sizeof(char) * size);
 	while (i < size)
 	{
 		p[i] = c;
