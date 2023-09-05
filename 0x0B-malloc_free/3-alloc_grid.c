fb9965b384494944f0a07b158fe6e6f8a33ca706
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-05 07:25:20 -0700

    task3 0x0B

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocate memory in a shape of grid
+ * @width: the width of the grid
+ * @height: the height of the grid
+ * Return: pointer to the grid start
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	p = malloc(sizeof(*p) * height);
+
+	if (p == NULL)
+		return (NULL);
+	while (i < width)
+	{
+		int j = 0;
+
+		p[i] = malloc(sizeof(**p) * width);
+		if (p[i] == NULL)
+		{
+			while (i--)
+				free(p[i]);
+			free(p);
+			return (NULL);
+		}
+		else
+		{
+			while (j < height)
+			{
+				p[i][j] = 0;
+				j++;
+			}
+		}
+		i++;
+	}
+	return (p);
+}
