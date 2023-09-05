a221b58baea6bbb5a88c6081c38ed0851be98651
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-05 14:08:05 -0700

    task 101.6 0x0B

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,7 +4,7 @@
 /**
  * _strlen - counts the length of characters of given number of words
  * @str: input string
- * @j; number of words
+ * @j: number of words
  * Return: number of characters of j words
  */
 int _strlen(char *str, int j)
@@ -21,6 +21,23 @@ int _strlen(char *str, int j)
 	return (count - (j + 1));
 }
 /**
+ * _size - get the number of words
+ * @str: input string
+ * Return: number of words
+ */
+int _size(char *str)
+{
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' && str[i + 1] != ' ')
+			size++;
+		i++;
+	}
+	if (!(str[i - 1] == ' ' && str[i] != ' '))
+		size++;
+	return (size);
+}
+/**
  * strtow - split string into word dynamilclly alocated int memory
  * @str: input string
  * Return: pointer to the new string
@@ -35,50 +52,34 @@ char **strtow(char *str)
 		return (NULL);
 	while (*str == ' ')
 		str++;
-	while (str[i] != '\0')
-	{
-		if (str[i] == ' ' && str[i + 1] != ' ')
-			size++;
-		i++;
-	}
-	if (!(str[i - 1] == ' ' && str[i] != ' '))
-		size++;
+	size = _size(str);
 	p = malloc(sizeof(char *) * size);
 	if (p == NULL || size == 0)
 		return (NULL);
 	while (j < size)
 	{
-		int k = 0;
-		int flag = 0;
+		int k = 0, flag = 0;
 
 		p[j] = malloc(sizeof(char) * _strlen(str, j));
 		if (p[j] == NULL)
-		{
-			while (j--)
-				free(p[j]);
-			free(p);
 			return (NULL);
-		}
-		else
+		while (str[c] == ' ' && str[c + 1] == ' ')
 		{
-			while (str[c] == ' ' && str[c + 1] == ' ')
-			{
-				c++;
-				flag = 1;
-			}
-			if (flag == 1)
-				c++;
-			while (str[c] != ' ')
-			{
-				p[j][k] = str[c];
-				n++;
-				c++;
-				k++;
-			}
-			p[j][k] = '\0';
+			c++;
+			flag = 1;
+		}
+		if (flag == 1)
+			c++;
+		while (str[c] != ' ')
+		{
+			p[j][k] = str[c];
 			n++;
 			c++;
+			k++;
 		}
+		p[j][k] = '\0';
+		n++;
+		c++;
 		j++;
 	}
 	p[j] = '\0';
