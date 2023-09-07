eda5a55a2c00221da51868495c32c6dc2e23391a
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-07 11:16:29 -0700

    task 3.1 0x0C

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,11 +10,12 @@
 
 int *array_range(int min, int max)
 {
-	int *p = malloc(sizeof(int) * (max - min) + 1);
+	int *p; 
 	int i = 0;
 
 	if (min > max)
 		return (NULL);
+	p = malloc(sizeof(int) * (max - min) + 1);
 	if (p == NULL)
 		return (NULL);
 	while (min <= max)
