8ba2c539501659bd92c97a389fa3b0011d61f151
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-07 09:17:24 -0700

    task 2.1 0x0C

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	int *p;
 	unsigned int i = 0;
 
-	if (size == 0)
+	if (size == 0 || nmemb == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
