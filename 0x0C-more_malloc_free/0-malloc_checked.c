a7b57f319ac0779a4d5c8f665d36514498039009
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-07 06:30:45 -0700

    task 0.1 0x0C

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
+ * malloc_checked - assigne memory locations
+ * @b: szie of the memory to be allocated
+ * Return: pointer to that location
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *p = malloc(b);
+
 	if (p == NULL)
 		exit(98);
 	return (p);
