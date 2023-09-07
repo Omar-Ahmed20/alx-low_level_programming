daf4c9d7c6bf0a16034c1ee014a3350805ae2482
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-07 08:47:42 -0700

    task 1.3 0x0C

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p = malloc(len1 + len2 + 1);
 	unsigned int i = 0, j = 0;
 
-	if (p == NULL || (len1 == 0 && len2 == 0))
+	if (p == NULL)
 		return (NULL);
 	while (i < len1)
 	{
