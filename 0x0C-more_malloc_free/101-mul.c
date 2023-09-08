c1998bc6aaab4681c50b8dbe7995a7a829c8ce65
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-08 10:25:26 -0700

    task 101.1 0x0C

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,141 +1,116 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
- * _strlen - count number of charachters
- * @s: input string
- * Return: count
+ * _isdigit - checks if character is digit
+ * @c: the charachter to be checked
+ * Return: 1 if digit 0 if not
  */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
 
+/**
+ * _strlen - count length of a string
+ * @s: input string
+ * Return: length of string
+ */
 int _strlen(char *s)
 {
 	int i = 0;
 
 	while (s[i] != '\0')
 		i++;
-
 	return (i);
 }
+
 /**
- * _printans - print the reversed answer in correct order
- * @str: sting of answer
- * @l1: length of first string
- * @l2: length of second string
+ * big_multiply - multiply two big number strings
+ * @s1: the first number string
+ * @s2: the second number string
+ * Return: the product of their maltuiplication
  */
-void _printans(int *str, int l1, int l2)
+char *big_multiply(char *s1, char *s2)
 {
-	int i = l1 + l2;
+	char *r;
+	int a, b, c, x, l1, l2;
 
-	while (i >= 0)
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	a = x = l1 + l2;
+	r = malloc(l1 + l2);
+	if (!r)
+		printf("Error\n"), exit(98);
+	while (a--)
+		r[a] = 0;
+	l1--;
+	while (l1 >= 0)
 	{
-		if (str[i] > 0)
-			break;
-		i--;
-	}
-
-	while (i >= 0)
-	{
-		_putchar(str[i] + 48);
-		i--;
-	}
-	_putchar('\n');
-}
-/**
- * multiply - multiply two big digits
- * @s1: first number string
- * @s2: second number string
- */
-void multiply(char *s1, char *s2)
-{
-	int i, j, tmp, l1 = _strlen(s1), l2 = _strlen(s2);
-	int a[1000], b[1000], ans[2000] = {0};
-
-	i = l1 - 1;
-	j = 0;
-	while (i >= 0)
-	{
-		a[j] = s1[i] - 48;
-		i--;
-		j++;
-	}
-	i = l2 - 1;
-	j = 0;
-	while (i >= 0)
-	{
-		b[j] = s2[i] - 48;
-		i--;
-		j++;
-	}
-	i = 0;
-	while (i < l2)
-	{
-		j = 0;
-		while (j < l1)
+		if (!_isdigit(s1[l1]))
 		{
-			ans[i + j] += b[i] * a[j];
-			j++;
+			free(r);
+			printf("Error\n");
+			exit(98);
 		}
-		i++;
-	}
-	i = 0;
-	while (i < l1 + l2)
-	{
-		tmp = ans[i] / 10;
-		ans[i] = ans[i] % 10;
-		ans[i + 1] = ans[i + 1] + tmp;
-		i++;
-	}
-	_printans(ans, l1, l2);
-}
-/**
- * _isdigit - check if the character is digit
- * @str: input string
- * Return: 1 if digit 0 if not
- */
-int _isdigit(char *str)
-{
-	int i = 0;
+		a = s1[l1] - 48;
+		c = 0;
+		l2 = _strlen(s2) - 1;
+		while (l2 >= 0)
+		{
+			if (!_isdigit(s2[l2]))
+			{
+				free(r);
+				printf("Error\n");
+				exit(98);
+			}
+			b = s2[l2] - 48;
 
-	while (str[i] != '\0')
-	{
-		int tmp = str[i] - 48;
+			c += r[l1 + l2 + 1] + (a * b);
+			r[l1 + l2 + 1] = c % 10;
 
-		if (tmp >= '0' && tmp <= '9')
-			return (0);
-		i++;
+			c /= 10;
+			l2--;
+		}
+		if (c)
+			r[l1 + l2 + 1] += c;
+		l1--;
 	}
-	return (1);
+	return (r);
 }
+
+
 /**
- * main - main function checks input with error handling
- * @argc: number of arguments to program
- * @argv: array of all arguments
- * Return: 0 if success 98 if fail
+ * main - multiply two big number strings
+ * @argc: the number of arguments
+ * @argv: array of all the arguments
+ * Return: Always 0 
  */
 int main(int argc, char **argv)
 {
-	char error[] = "Error\n";
-	int i;
+	char *res;
+	int a, c, x;
 
 	if (argc != 3)
 	{
-		i = 0;
-		while (error[i] != '\0')
-		{
-			_putchar(error[i]);
-			i++;
-		}
-		exit(98);
+		printf("Error\n");
+	       	exit(98);
 	}
-	else if (!(_isdigit(argv[1]) && _isdigit(argv[2])))
+	x = _strlen(argv[1]) + _strlen(argv[2]);
+	res = big_multiply(argv[1], argv[2]);
+	c = 0;
+	a = 0;
+	while (c < x)
 	{
-		i = 0;
-		while (error[i] != '\0')
-		{
-			_putchar(error[i]);
-			i++;
-		}
-		exit(98);
+		if (res[c])
+			a = 1;
+		if (a)
+			_putchar(res[c] + 48);
+		c++;
 	}
-	multiply(argv[1], argv[2]);
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(res);
 	return (0);
 }
