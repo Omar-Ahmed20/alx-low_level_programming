5ee5efe7caf4ca48f385c3b08bc3b6faf1295ff2
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-11 16:49:49 -0700

    task 0 0x0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif /*DOG_H*/
