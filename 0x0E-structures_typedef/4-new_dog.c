76026013567883bb91e76b07e6cbf32e1a1f1092
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-11 17:40:18 -0700

    task 4.1 0x0E

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,38 @@
 #include "dog.h"
 #include <stdlib.h>
+/**
+ * _strcpy - coppy an array to another array
+ * @dest: first array
+ * @src: second array
+ * Return: the array
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
 
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * _strlen - count number of charachters
+ * @s: input string
+ * Return: count
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
 /**
  * new_dog - creats new dog with the new datatype
  * @name: name of the dog
@@ -11,13 +43,29 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p = malloc(sizeof(dog_t));
+	dog_t *p;
 
+	if (age < 0 || !owner || !name)
+		return (NULL);
+	p = malloc(sizeof(dog_t));
 	if (p == NULL)
 		return (NULL);
-	p->name = name;
+	p->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (p->name == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	p->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (p->owner == NULL)
+	{
+		free(p->name);
+		free(p);
+		return (NULL);
+	}
+	p->name = _strcpy(p->name, name);
 	p->age = age;
-	p->owner = owner;
+	p->owner = _strcpy(p->owner, owner);
 
 	return (p);
 }
