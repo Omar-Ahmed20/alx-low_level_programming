3111b66552fd44b644d0be2dbe4c63a124bfdb94
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-11 17:11:52 -0700

    task 2 0x0E

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print the struct
+ * @d: pointer to the struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %s\n", d->name);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
