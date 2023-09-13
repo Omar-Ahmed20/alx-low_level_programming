9d3e72ed3ed511172f30f56878aac8703389cc9a
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-13 12:01:28 -0700

    task 0 0x0F

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print out name
+ * @name: the name to be printed
+ * @f: pointer to a function that print name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
