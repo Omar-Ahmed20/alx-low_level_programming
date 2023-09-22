fda56e65cfac12238f405b4bc675dbecf20a94f0
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-21 19:02:28 -0700

    task 100.1 0x12

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
-
 /**
  * print - print before main
  */
 void print(void)
 {
-	printf("You're beat! and yet, you must allow,
+	printf("You're beat! and yet, you must allow,\
 			\nI bore my house upon my back!\n");
 }
