3ae63ff8523a26aaa0c1e7856edd319fd258f61e
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-26 07:31:55 -0700

    task 5 0x13

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free memory of linked list
+ * @head: pointer to the first node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *t;
+	listint_t *temp = *head;
+
+	while (temp)
+	{
+		t = temp;
+		temp = temp->next;
+		free(t);
+	}
+	*head = NULL;
+}
