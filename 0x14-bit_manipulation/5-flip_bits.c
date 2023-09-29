330cebdb10a5566d7b0241884b4a61e5ceec5169
Omar Ahmed <omarahmedhu8@gmail.com>
2023-09-29 10:09:43 -0700

    task 5.1 0x14

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
+ * flip_bits - number of bits need to be flipped to get m
+ * @n: input number
+ * @m: number to be converted to
+ * Return: number of bits to be flipped
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
