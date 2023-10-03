9ea63577725427e991414eb685b82b2556852ec6
Omar Ahmed <omarahmedhu8@gmail.com>
2023-10-03 11:35:26 -0700

    task 1 0x15

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strlen - count number of characters of a string
+ * @str: input string
+ * Return:: lenght of the input string
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	if (str == NULL)
+		return (0);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * create_file - createa new file and write content
+ * @filename: name of the file
+ * @text_content: content to write in the file
+ * Return: 1 if success -1 if fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int written, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+		return (-1);
+	if (len)
+		written = write(fd, text_content, len);
+	close(fd);
+	return (written == len ? 1 : -1);
+}
